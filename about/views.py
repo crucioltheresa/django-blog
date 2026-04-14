4c9ab810cbf19160c7f25bdd7ce1c02d961771b3
crucioltheresa <mtcruciol@icloud.com>
2026-04-14 19:51:56 +0100

    feat(about): implement collaboration request form and success message on about page

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -13,6 +14,18 @@ def about_me(request):
     **Template**
         :template:`about/about.html`
     """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! "
+                "I endeavour to respond within 2 working days.",
+            )
+
     about = About.objects.all().order_by("-updated_on").first()
     collaborate_form = CollaborateForm()
 
