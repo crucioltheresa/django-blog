617aceaf504762fb7d458d851b114441160a7ddf
crucioltheresa <mtcruciol@icloud.com>
2026-04-14 17:46:16 +0100

    feat(post): add comments section with count and display in post detail view

--- blog/views.py
+++ blog/views.py
@@ -21,9 +21,16 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post, "coder": "Maria Theresa Cruciol"},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "coder": "Maria Theresa Cruciol",
+        },
     )
