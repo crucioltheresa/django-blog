a0529c4a6b7cbf2435a5cf99e30b0a9f89f23445
crucioltheresa <mtcruciol@icloud.com>
2026-04-20 18:11:04 +0100

    fix(comments): update event handling for comment ID retrieval and modal initialization

--- static/js/comments.js
+++ static/js/comments.js
@@ -3,13 +3,12 @@ const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
-const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
 /**
 * Initializes edit functionality for the provided edit buttons.
-* 
+*
 * For each button in the `editButtons` collection:
 * - Retrieves the associated comment's ID upon click.
 * - Fetches the content of the corresponding comment.
@@ -19,7 +18,7 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 */
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("data-comment_id");
+        let commentId = e.currentTarget.getAttribute("data-comment_id");
         let commentContent = document.getElementById(`comment${commentId}`).innerText;
         commentText.value = commentContent;
         submitButton.innerText = "Update";
@@ -29,18 +28,19 @@ for (let button of editButtons) {
 
 /**
 * Initializes deletion functionality for the provided delete buttons.
-* 
+*
 * For each button in the `deleteButtons` collection:
 * - Retrieves the associated comment's ID upon click.
-* - Updates the `deleteConfirm` link's href to point to the 
+* - Updates the `deleteConfirm` link's href to point to the
 * deletion endpoint for the specific comment.
-* - Displays a confirmation modal (`deleteModal`) to prompt 
+* - Displays a confirmation modal (`deleteModal`) to prompt
 * the user for confirmation before deletion.
 */
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("data-comment_id");
+        let commentId = e.currentTarget.getAttribute("data-comment_id");
         deleteConfirm.href = `delete_comment/${commentId}`;
+        const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
         deleteModal.show();
     });
 }
